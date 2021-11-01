fa97214aecdf804f94c86b2709f7f7c559ba2a95
Andy Freeland <andy@andyfreeland.net>
2021-10-31 20:14:37 -0700

    requests -> httpx async

--- src/wanikani_apprentice/wanikani.py
+++ src/wanikani_apprentice/wanikani.py
@@ -1,8 +1,8 @@
+from collections.abc import AsyncIterable
 import enum
-import typing
 
 import attr
-import requests
+import httpx
 
 APPRENTICE_SRS_STAGES = [1, 2, 3, 4]
 
@@ -18,48 +18,51 @@ class WaniKaniAPIClient:
     BASE_URL = "https://api.wanikani.com/v2"
 
     api_key: str = attr.field(repr=False)
-    session: requests.Session = attr.field(factory=requests.Session)
+    client: httpx.AsyncClient = attr.field(factory=httpx.AsyncClient)
 
     def __attrs_post_init__(self):
-        self.session.headers.update(
+        self.client.headers.update(
             {
                 "Authorization": f"Bearer {self.api_key}",
                 "Wanikani-Revision": "20170710",
             },
         )
 
-    def assignments(self) -> list[dict]:
+    async def assignments(self) -> list[dict]:
         """Get all Apprentice assignments"""
         # TODO: Handle possible (but unlikely) pagination
-        return self.session.get(
+        resp = await self.client.get(
             f"{self.BASE_URL}/assignments",
             params={
                 "srs_stages": ",".join(str(stage) for stage in APPRENTICE_SRS_STAGES),
                 "hidden": "false",
             },
-        ).json()["data"]
+        )
+        return resp.json()["data"]
 
-    def _subjects(self, subject_type: SubjectType) -> typing.Iterator[dict]:
+    async def _subjects(self, subject_type: SubjectType) -> AsyncIterable[dict]:
         next_url = f"{self.BASE_URL}/subjects"
 
         while next_url is not None:
-            resp = self.session.get(
+            resp = await self.client.get(
                 next_url,
-                params={"types": subject_type, "hidden": "false"},
-            ).json()
+                params={"types": subject_type.value, "hidden": "false"},
+            )
+            resp = resp.json()
 
             next_url = resp["pages"]["next_url"]
 
-            yield from resp["data"]
+            for subject in resp["data"]:
+                yield subject
 
-    def radicals(self) -> list[dict]:
+    async def radicals(self) -> AsyncIterable[dict]:
         """Get all radicals"""
         return self._subjects(SubjectType.RADICAL)
 
-    def kanji(self) -> typing.Iterator[dict]:
+    async def kanji(self) -> AsyncIterable[dict]:
         """Get all kanji"""
         return self._subjects(SubjectType.KANJI)
 
-    def vocabulary(self) -> typing.Iterator[dict]:
+    async def vocabulary(self) -> AsyncIterable[dict]:
         """Get all vocabulary"""
         return self._subjects(SubjectType.VOCABULARY)
