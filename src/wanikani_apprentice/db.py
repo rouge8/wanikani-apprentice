6ba03fba1ed2347251900ed8d07f90285783be1a
Andy Freeland <andy@andyfreeland.net>
2021-11-01 11:12:40 -0700

    Minimal concurrency in `populate_db()`
    ```
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?types=radical&hidden=false "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?types=kanji&hidden=false "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?types=vocabulary&hidden=false "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=1439&types=kanji "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=2439&types=kanji "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=3469&types=vocabulary "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=4477&types=vocabulary "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=5479&types=vocabulary "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=6483&types=vocabulary "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=7484&types=vocabulary "HTTP/1.1 200 OK"
    DEBUG:httpx._client:HTTP Request: GET https://api.wanikani.com/v2/subjects?hidden=false&page_after_id=8490&types=vocabulary "HTTP/1.1 200 OK"
    ```

--- src/wanikani_apprentice/db.py
+++ src/wanikani_apprentice/db.py
@@ -1,3 +1,4 @@
+import asyncio
 import typing
 
 import attr
@@ -21,9 +22,25 @@ DB = Database()
 
 
 async def populate_db(api: "WaniKaniAPIClient") -> None:
+    radicals = asyncio.create_task(_populate_radicals(api))
+    kanji = asyncio.create_task(_populate_kanji(api))
+    vocabulary = asyncio.create_task(_populate_vocabulary(api))
+
+    await vocabulary
+    await kanji
+    await radicals
+
+
+async def _populate_radicals(api: "WaniKaniAPIClient") -> None:
     async for radical in api.radicals():
         DB.radical[radical.id] = radical
+
+
+async def _populate_kanji(api: "WaniKaniAPIClient") -> None:
     async for kanji in api.kanji():
         DB.kanji[kanji.id] = kanji
+
+
+async def _populate_vocabulary(api: "WaniKaniAPIClient") -> None:
     async for vocabulary in api.vocabulary():
         DB.vocabulary[vocabulary.id] = vocabulary
