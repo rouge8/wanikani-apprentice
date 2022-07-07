cb9f3c89aaf2b4755af5dad9ae602f8103049b1c
Andy Freeland <andy@andyfreeland.net>
2022-07-06 20:48:06 -0700

    Initial web app with logging and a `/test-500` route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use axum::{routing::get, Router};
 use config::Config;
 use db::Database;
 use dotenvy::dotenv;
+use std::net::SocketAddr;
+use tower::ServiceBuilder;
+use tower_http::catch_panic::CatchPanicLayer;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 use wanikani::WaniKaniAPIClient;
 
@@ -8,6 +14,25 @@ mod config;
 mod db;
 mod models;
 mod wanikani;
+
+async fn test_500() {
+    let _ = 1 / 0;
+}
+
+fn create_app() -> Router {
+    Router::new().route("/test-500", get(test_500)).layer(
+        ServiceBuilder::new().layer(CatchPanicLayer::new()).layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(tower_http::LatencyUnit::Seconds),
+                ),
+        ),
+    )
+}
 
 #[tokio::main]
 async fn main() -> reqwest::Result<()> {
@@ -18,16 +43,39 @@ async fn main() -> reqwest::Result<()> {
 
     let config = Config::from_env();
 
-    let client = WaniKaniAPIClient::new(&config.wanikani_api_key, reqwest::Client::new());
+    let api = WaniKaniAPIClient::new(&config.wanikani_api_key, reqwest::Client::new());
 
-    let username = client.username().await?;
-    println!("Welcome, {username}!");
-
+    // Load the WaniKani data
     let mut db = Database::new();
-    db.populate(&client).await?;
+    db.populate(&api).await?;
 
-    let assignments = client.assignments(&db).await?;
-    println!("You have {} assignments.", assignments.len());
+    // Build the application
+    let app = create_app();
+
+    // Serve the app
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    info!("listening on http://{addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::http::StatusCode;
+    use axum_test_helper::TestClient;
+    use pretty_assertions::assert_eq;
+
+    #[tokio::test]
+    async fn test_500() {
+        let app = create_app();
+        let test_client = TestClient::new(app);
+
+        let resp = test_client.get("/test-500").send().await;
+        assert_eq!(resp.status(), StatusCode::INTERNAL_SERVER_ERROR);
+    }
 }
