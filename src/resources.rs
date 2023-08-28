18eda8a5b19d537da4e6997ef344ee18ec9e8ebd
Andy Freeland <andy@andyfreeland.net>
2023-08-28 11:58:45 -0700

    Upgrade minijinja

--- src/resources.rs
+++ src/resources.rs
@@ -1,5 +1,5 @@
 use include_dir::{include_dir, Dir};
-use minijinja::{Environment, Source};
+use minijinja::Environment;
 use once_cell::sync::Lazy;
 
 use crate::display_time_remaining;
@@ -9,17 +9,14 @@ pub static STATIC_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static");
 static TEMPLATES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates");
 pub static TEMPLATES: Lazy<Environment> = Lazy::new(|| {
     let mut env = Environment::new();
-    let mut source = Source::new();
     let templates = TEMPLATES_DIR
         .files()
         .map(|file| (file.path().to_string_lossy(), file.contents_utf8().unwrap()))
         .collect::<Vec<_>>();
     for (path, template) in templates {
-        source
-            .add_template(path, template)
+        env.add_template_owned(path, template)
             .expect("Unable to add template");
     }
-    env.set_source(source);
 
     env.add_filter("display_time_remaining", display_time_remaining);
     env
